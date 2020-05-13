5d41214ea02ddb6d427cdc4ac391ef181aac7322
Kaihup <kai_91@live.nl>
2020-05-13 10:14:27 +0200

    first commit

---
+++ src/test/java/nl/group/wms/WmsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.group.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
