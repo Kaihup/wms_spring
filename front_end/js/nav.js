1a806534c4ff6ce8e457932423d2c8975adfa052
Kaihup <kai_91@live.nl>
2020-05-19 17:11:35 +0200

    next

---
+++ front_end/js/nav.js
@@ -0,0 +1,11 @@
+function navload() {
+	$("#link-login").click(function () {
+		$("#page-content").load("pages/login.html");
+		console.log("page login");
+	});
+
+	$("#link-catalog").click(function () {
+		$("#page-content").load("pages/catalog.html");
+		console.log("page catalog");
+	});
+}
