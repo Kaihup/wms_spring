69dcef6fdf92076d6f4545fa7835c1208b5a5f05
Kaihup <kai_91@live.nl>
2020-05-19 20:29:53 +0200

    next

--- front_end/core-structure/html-loader.js
+++ front_end/core-structure/html-loader.js
@@ -2,23 +2,15 @@
 $(document).ready(function () {
 	$("#header").load("core-structure/header.html", navload);
 	$("#footer").load("core-structure/footer.html");
-<<<<<<< HEAD
-	// $("#page-content")
-	// showProducts();
 });
-=======
+
 //	$("#login").load("")
 //	$("#page-content").load("pages/catalog.html");
 //	showProducts();
-	//alert(document.getElementById("#link-login"));
-	/*
+//alert(document.getElementById("#link-login"));
+/*
 	$("#link-login").click(function() {
 		$("#page-content").load("pages/login.html");
 		console.log("Het werkt");
   });
   */
-
-
-
-});
->>>>>>> master
--- front_end/js/nav.js
+++ front_end/js/nav.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function navload() {
 	$("#link-login").click(function () {
 		$("#page-content").load("pages/login.html");
@@ -8,28 +7,12 @@ function navload() {
 	$("#link-catalog").click(function () {
 		$("#page-content").load("pages/catalog.html");
 		console.log("page catalog");
+		showProducts();
+	});
+
+	$("#link-registercustomer").click(function () {
+		alert("boe");
+		$("#page-content").load("pages/register-customer.html");
+		console.log("customer register");
 	});
 }
-=======
-function navload(){
-    $("#link-login").click(function() {
-		$("#page-content").load("pages/login.html");
-		console.log("page login");
-  });
-
-  $("#link-catalog").click(function() {
-    $("#page-content").load("pages/catalog.html");
-    console.log("page catalog");
-    showProducts();
-  });
-
-  $("#link-registercustomer").click(function(){
-    alert("boe");
-    $("#page-content").load("pages/register-customer.html");
-    console.log("customer register");
-  });
-
-
-
-}
->>>>>>> master
