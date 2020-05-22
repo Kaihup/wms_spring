748991cfcc1bbde79a62df43bea768ef8fd843d3
Kaihup <kai_91@live.nl>
2020-05-22 12:14:23 +0200

    angular

--- front_end/angular-wms/e2e/src/app.e2e-spec.ts
+++ front_end/angular-wms/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('voorbeeld-ata app is running!');
+    expect(page.getTitleText()).toEqual('angular-wms app is running!');
   });
 
   afterEach(async () => {
