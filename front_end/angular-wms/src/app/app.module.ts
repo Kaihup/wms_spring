bf3f09eaec723142ce05cea802a9146df794d15e
Kaihup <kai_91@live.nl>
2020-05-22 15:41:08 +0200

    angular

--- front_end/angular-wms/src/app/app.module.ts
+++ front_end/angular-wms/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
+import { ProductComponent } from './product/product.component'; // Product Component
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ 
-    BrowserModule,
-    FormsModule
-  ],
-  declarations: [ AppComponent, StudentComponent ]
+  declarations: [AppComponent, ProductComponent], // Eigen werk
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule], // Library modules
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ front_end/angular-wms/src/app/product/product.ts
@@ -0,0 +1,5 @@
+export class Product {
+  id?: number; // ? geeft primary key aan
+  naam: string;
+  prijs: number;
+}
